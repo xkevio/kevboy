902c24d26f6633ff60cc7240a3c1dc48fe76ebe1
xkevio <xkeviota@gmail.com>
2023-01-31 02:59:32 +0100

    Fix joypad interrupts and deduplicate handling

--- src/input/joypad.rs
+++ src/input/joypad.rs
@@ -7,17 +7,22 @@ use crate::{
 
 pub struct Joypad {
     joyp: u8,
+    prev_joyp: u8,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum ButtonType {
     Action,
     Direction,
+    None,
 }
 
 impl Default for Joypad {
     fn default() -> Self {
-        Self { joyp: 0xCF }
+        Self {
+            joyp: 0xCF,
+            prev_joyp: 0xCF,
+        }
     }
 }
 
@@ -36,14 +41,16 @@ impl Joypad {
         self.reset_pressed_keys();
 
         match self.get_button_type() {
-            Some(ButtonType::Action) => {
-                self.handle_key_input(ctx, interrupt_handler, Key::P, Key::O, Key::Q, Key::Enter)
-            }
-            Some(ButtonType::Direction) => {
-                self.handle_key_input(ctx, interrupt_handler, Key::D, Key::A, Key::W, Key::S)
-            }
-            None => {}
+            ButtonType::Action => self.handle_key_input(ctx, &[Key::P, Key::O, Key::Q, Key::Enter]),
+            ButtonType::Direction => self.handle_key_input(ctx, &[Key::D, Key::A, Key::W, Key::S]),
+            _ => {}
         }
+
+        if (self.prev_joyp & 0xF == 0xF) && (self.joyp & 0xF != 0xF) {
+            interrupt_handler.request_interrupt(Interrupt::Joypad);
+        }
+
+        self.prev_joyp = self.joyp;
     }
 
     pub fn reset_pressed_keys(&mut self) {
@@ -57,55 +64,21 @@ impl Joypad {
     /// `key3`: Shift or Up
     ///
     /// `key4`: Start or Down
-    fn handle_key_input(
-        &mut self,
-        ctx: &Context,
-        interrupt_handler: &mut InterruptHandler,
-        key1: Key,
-        key2: Key,
-        key3: Key,
-        key4: Key,
-    ) {
-        if ctx.input().key_down(key1) {
-            if self.joyp & 0x1 != 0 {
-                interrupt_handler.request_interrupt(Interrupt::Joypad);
+    fn handle_key_input(&mut self, ctx: &Context, keys: &[Key; 4]) {
+        for (bit, key) in keys.iter().enumerate() {
+            if ctx.input().key_down(*key) {
+                self.joyp &= !(0x1 * (2u8.pow(bit as u32)));
             }
-
-            self.joyp &= !(0x1);
-        }
-
-        if ctx.input().key_down(key2) {
-            if self.joyp & 0x2 != 0 {
-                interrupt_handler.request_interrupt(Interrupt::Joypad);
-            }
-
-            self.joyp &= !(0x2);
-        }
-
-        if ctx.input().key_down(key3) {
-            if self.joyp & 0x4 != 0 {
-                interrupt_handler.request_interrupt(Interrupt::Joypad);
-            }
-
-            self.joyp &= !(0x4);
-        }
-
-        if ctx.input().key_down(key4) {
-            if self.joyp & 0x8 != 0 {
-                interrupt_handler.request_interrupt(Interrupt::Joypad);
-            }
-
-            self.joyp &= !(0x8);
         }
     }
 
-    fn get_button_type(&self) -> Option<ButtonType> {
+    fn get_button_type(&self) -> ButtonType {
         if self.joyp & 0x20 == 0 {
-            Some(ButtonType::Action)
+            ButtonType::Action
         } else if self.joyp & 0x10 == 0 {
-            Some(ButtonType::Direction)
+            ButtonType::Direction
         } else {
-            None
+            ButtonType::None
         }
     }
 }
